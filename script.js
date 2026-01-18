b1d8fe6dfbf214c940b3dcd07d4932c3f5119c23
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-18 15:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
-    //Write your code here
-  
+let radiusInput = document.getElementById("radius")
+let valumeInput = document.getElementById("volume")
+let h3 = document.querySelector("h3")
+
+function volume_sphere(e) {
+	e.preventDefault()
+		let radius = radiusInput.value
+	
+		let calVolume = (4/3) * (Math.PI * radius**3)
+	
+		h3.innerText = calVolume.toFixed(4)
+		volume.textContent = calVolume.toFixed(4)
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
